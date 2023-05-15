d87a97c432762bdf0c7b4d826f60a69f38d35e03
bernardperru <47976460+bernardperru@users.noreply.github.com>
2023-05-15 12:18:49 +0200

    Skeleton for traintravel implemented

---
+++ src/com/Train/TrainTravel.java
@@ -0,0 +1,54 @@
+package com.Train;
+
+public class TrainTravel {
+    public boolean onTrain, inToilet, caught, arrived, destination, prison;
+    public int x,y,g;
+
+    public void buyTicket() {
+
+    }
+
+    public void enterToilet() {
+
+    }
+
+    public void exitToilet() {
+
+    }
+
+    public void enterTrain() {
+
+    }
+
+    public void exitTrain() {
+
+    }
+
+    public void walk() {
+
+    }
+
+    public void arrived() {
+
+    }
+
+    public void caught() {
+
+    }
+
+    public void destination() {
+
+    }
+
+    public void prison() {
+
+    }
+
+    public void ticketCheck() {
+
+    }
+
+    public void wait(int delay) {
+
+    }
+}
